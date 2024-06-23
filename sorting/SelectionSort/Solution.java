79acf6bf9df0149917c20d8d2e0047c1cccad9a7
Jinang12 <156568628+Jinang12@users.noreply.github.com>
2024-06-23 18:59:00 +0530

    Create Solution.java ar sorting/SelectionSort

---
+++ sorting/SelectionSort/Solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Solution{
+    public static int[] selectionSort(int nums[]){
+        for(int i=0;i<nums.length;i++){
+                int max=Math.max(0,nums[nums.length-1-i]);
+                int maxIndex=0;
+                for(int j=1;j<nums.length-i;j++){
+                    if(nums[j]> nums[maxIndex]){
+                        maxIndex=j;
+                    }
+                }
+                int temp=nums[nums.length-1-i];
+                nums[nums.length-1-i]=nums[maxIndex];
+                nums[maxIndex]=temp;
+        }
+        return nums;
+    }
+    public static void main(String args[]){
+        int nums[]={4,1,3,9,2,0};
+        System.out.println(Arrays.toString(selectionSort(nums)));
+    }
+}
