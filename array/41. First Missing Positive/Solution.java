634b119124954146b070a261ca4cbe6d3c211227
Jinang12 <156568628+Jinang12@users.noreply.github.com>
2024-06-25 20:05:07 +0530

    Create Solution.java at array/41. Find Missing Positive

---
+++ array/41. First Missing Positive/Solution.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public static int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            } else {
+                i++;
+            }
+        }
+        for (i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int nums[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int answer = firstMissingPositive(nums);
+        System.out.println(answer);
+    }
+}
